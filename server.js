ae0784f83b1ffcfb39d1c007619b96a59db23909
Ben Stokoe <benstokoe1@googlemail.com>
2016-02-04 21:49:18 +0000

    Try this server

--- server.js
+++ server.js
@@ -2,18 +2,11 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-app.use(express.static('dist'));
-app.use('/bundle.min.js', function (req, res) {
-  return res.sendFile(path.join(__dirname, 'bundle.min.js'));
-});
-app.use('/css/app.css', function (req, res) {
-  return res.sendFile(path.join(__dirname, 'css/app.css'));
-});
-app.use('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(process.env.PORT || 3000, function (err) {
+var static_path = path.join(__dirname, 'dist');
+app.use(express.static(static_path))
+.get('/', function (req, res) {
+  res.sendFile('index.html')
+.listen(process.env.PORT || 3000, function (err) {
   if (err) { console.log(err)  };
-  console.log('Server started');
+  console.log('Started server');
 });
