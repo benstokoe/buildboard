458ecc63254e2139c6da52ecfe2b5dc47238d6f6
Ben Stokoe <benstokoe1@googlemail.com>
2016-02-04 21:42:15 +0000

    Needs to be in dependencies for postinstall

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ var path = require('path');
 
 var app = express();
 app.use(express.static('dist'));
+app.use('/bundle.min.js', function (req, res) {
+  return res.sendFile(path.join(__dirname, 'bundle.min.js'));
+});
+app.use('/css/app.css', function (req, res) {
+  return res.sendFile(path.join(__dirname, 'css/app.css'));
+});
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(process.env.PORT || 3000, function (err) {
   if (err) { console.log(err)  };
