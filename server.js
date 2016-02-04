f4e8e0ef84b3aa8e549418859b12a2bf76184f7e
Ben Stokoe <benstokoe1@googlemail.com>
2016-02-04 21:50:54 +0000

    Fix server

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var path = require('path');
 
 var app = express();
 var static_path = path.join(__dirname, 'dist');
+
 app.use(express.static(static_path))
-.get('/', function (req, res) {
-  res.sendFile('index.html')
-.listen(process.env.PORT || 3000, function (err) {
-  if (err) { console.log(err)  };
-  console.log('Started server');
-});
+  .get('/', function (req, res) {
+    res.sendFile('index.html')
+  })
+  .listen(process.env.PORT || 3000, function (err) {
+    if (err) { console.log(err)  };
+    console.log('Started server');
+  });
