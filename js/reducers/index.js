4308712e6259b9a2c412e801c4b6c589804d1e61
Ben Stokoe <benstokoe1@googlemail.com>
2016-03-06 17:30:36 +0000

    Save to localstorage!

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,10 +1,11 @@
+import { reducer } from 'redux-storage';
 import { combineReducers } from 'redux';
 import projects from './projects';
 import settings from './settings';
 
-const rootReducer = combineReducers({
+const rootReducer = reducer(combineReducers({
   projects,
   settings
-});
+}));
 
 export default rootReducer;
