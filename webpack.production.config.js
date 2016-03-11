1601699e80a704ee065be4d6fb44caa6483d93bf
Ben Stokoe <benstokoe1@googlemail.com>
2016-03-11 08:28:47 +0000

    Didn't change production webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
-      { test: /\.sass$/, loader: ExtractTextPlugin.extract('css!sass?indentedSyntax') },
+      { test: /\.(scss|css)$/, loader: ExtractTextPlugin.extract('css!postcss-loader!sass') },
       { test: /\.(svg|woff2|eot|ttf|woff)$/, loader: 'url-loader?limit=100000'  }
     ]
   },
