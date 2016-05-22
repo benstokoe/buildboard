c919ae3e84c8f02f107602526e1b64cd144c4cf5
Ben Stokoe <benstokoe1@googlemail.com>
2016-05-22 11:36:41 +0100

    Get it working on heroku again

--- js/testSetup.js
+++ js/testSetup.js
@@ -1,8 +1,3 @@
-require('babel-core/register')({
-  presets: ['es2015', 'stage-0', 'react'],
-  plugins: ['transform-decorators-legacy', 'add-module-exports']
-});
-
 import jsdom from 'jsdom';
 
 global.window = { document: global.document, location: { search: '?circleToken=8888' } };
