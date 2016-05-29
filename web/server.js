8c73e9f4bf8fb8815a23814ace66e02f3a381810
Ben Stokoe <benstokoe1@googlemail.com>
2016-05-29 11:22:29 +0100

    Try this

--- web/server.js
+++ web/server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function (req, res) {
-  res.render('index.html');
+  res.sendFile('index.html', { root: __dirname } );
 });
 
 app.listen(process.env.PORT || 3000, function (err) {
