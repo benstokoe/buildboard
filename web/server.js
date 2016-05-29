b2285a3d375405f01ff89f88b272ad24b5326acd
Ben Stokoe <benstokoe1@googlemail.com>
2016-05-29 11:14:46 +0100

    Try this now

--- web/server.js
+++ web/server.js
@@ -1,14 +1,13 @@
 var express = require('express');
-var path = require('path');
-
 var app = express();
-var static_path = path.join('dist');
 
-app.use(express.static(static_path))
-  .get('/', function (req, res) {
-    res.sendFile('index.html', { root: __dirname } )
-  })
-  .listen(process.env.PORT || 3000, function (err) {
-    if (err) { console.log(err)  };
-    console.log('Started server');
-  });
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+});
+
+app.listen(process.env.PORT || 3000, function (err) {
+  if (err) { console.log(err)  };
+  console.log('Started server');
+});
